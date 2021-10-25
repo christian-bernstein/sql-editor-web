2c42cdfe9f5621e3306c2bdd84466126057ae27d
Chris <christian.bernsteinde@gmail.com>
2021-10-25 12:39:58 +0200

    unnamed

---
+++ src/packets/in/ProtocolSwitchPacket.ts
@@ -0,0 +1,6 @@
+/**
+ * Switch the connectors protocol. Used in the connectors base-protocol.
+ */
+export type ProtocolSwitchPacket = {
+    newProtocol: string
+}
---
+++ src/packets/out/LoginRequestByCredentialsPacket.ts
@@ -0,0 +1,7 @@
+/**
+ * Send all the data, required for a successful login procedure.
+ */
+export type LoginRequestByCredentialsPacket = {
+    username: string,
+    password: string,
+}
