355ca0d2e4674664456fe38e1bedd9446f93eca0
Chris <christian.bernsteinde@gmail.com>
2021-12-17 00:13:35 +0100

    unnamed

--- src/tests/custodian/CustodianPage.tsx
+++ src/tests/custodian/CustodianPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {App} from "../../logic/App";
 import {Themeable} from "../../Themeable";
 import {Environment} from "../../logic/Environment";
+import {BrowserRouter, Redirect, Route} from "react-router-dom";
 
 export type CustodianPageProps = {
     appTitle: string
@@ -21,18 +22,24 @@ export class CustodianPage extends React.Component<CustodianPageProps, Custodian
 
     render() {
         return (
-            <div>
-
+            <div className={"custodian-page"}>
+                <BrowserRouter>
+                    <Route path={"/"} render={props => <Redirect push to={App.app().config.defaultAppRoute}/>}/>
+                    <Route path={"/select"} render={props => <>choose subject</>}/>
+                    <Route path={"/select/:subject"} render={props => <>choose course</>}/>
+                    <Route path={"/:subject/:course"} render={props => <>choose course</>}/>
+                </BrowserRouter>
             </div>
         );
     }
 }
 
+
 export function initApp() {
     // noinspection DuplicatedCode
     App.appOrCreate({
         debugMode: true,
-        defaultAppRoute: "/boarding",
+        defaultAppRoute: "select",
         defaultDebugAppRoute: "/chart",
         themes: new Map<string, Themeable.Theme>([["dark-green", Themeable.defaultTheme]]),
         defaultTheme: "dark-green",
