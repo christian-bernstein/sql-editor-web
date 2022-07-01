8f868063bb2a9feecf43c42f8f9f2a1044a961b7
Chris <christian.bernsteinde@gmail.com>
2022-07-01 14:52:42 +0200

    unnamed

--- src/pages/editor/tabs/scripting/DatabaseScriptingTab.tsx
+++ src/pages/editor/tabs/scripting/DatabaseScriptingTab.tsx
@@ -1,19 +1,19 @@
 import {BernieComponent} from "../../../../logic/BernieComponent";
-import {ProjectInfoData} from "../../../../logic/data/ProjectInfoData";
 import {Assembly} from "../../../../logic/assembly/Assembly";
 import {Themeable} from "../../../../logic/style/Themeable";
 import {Text} from "../../../../components/lo/Text";
-import {DimensionalMeasured, percent} from "../../../../logic/style/DimensionalMeasured";
-import {Screen} from "../../../../components/lo/Page";
-import {Centered} from "../../../../components/lo/PosInCenter";
+import {percent, px} from "../../../../logic/style/DimensionalMeasured";
 import Editor from "@monaco-editor/react";
 import {FormDataHub} from "../../../../tests/epicure/components/FormDataHub";
 import _ from "lodash";
 import {getOr} from "../../../../logic/Utils";
 import {Button} from "../../../../components/lo/Button";
 import {FlexBox} from "../../../../components/lo/FlexBox";
-import {Dimension} from "../../../../logic/style/Dimension";
 import {TabProps} from "../TabProps";
+import {ScriptingAPI} from "../../../../logic/scripting/ScriptingAPI";
+import {ObjectVisualMeaning} from "../../../../logic/style/ObjectVisualMeaning";
+import {ReactComponent as RunIcon} from "../../../../assets/icons/ic-16/ic16-play.svg";
+import {Icon} from "../../../../components/lo/Icon";
 
 export type DatabaseScriptingTabProps = TabProps & {
 }
@@ -40,12 +40,16 @@ export class DatabaseScriptingTab extends BernieComponent<DatabaseScriptingTabPr
     componentRender(p: DatabaseScriptingTabProps, s: any, l: DatabaseScriptingTabLocalState, t: Themeable.Theme, a: Assembly): JSX.Element | undefined {
         return (
             <FlexBox height={percent(100)} width={percent(100)}>
-                <Button children={
-                    <Text text={"execute"}/>
+                <Button visualMeaning={ObjectVisualMeaning.INFO} opaque children={
+                    <Text text={"run"} enableRightAppendix rightAppendix={
+                        <Icon icon={<RunIcon/>} size={px(18)}/>
+                    }/>
                 } onClick={() => {
                     const code: string = l.fdh.get("code", "");
                     if (code.length > 0) {
-                        new Function("editor", code)(p.editor);
+                        new Function("api", code)(
+                            new ScriptingAPI(p.editor, p.projectInfo)
+                        );
                     }
                 }}/>
                 <FlexBox width={percent(100)} style={{
@@ -83,11 +87,11 @@ export class DatabaseScriptingTab extends BernieComponent<DatabaseScriptingTabPr
                             const libUri = 'ts:filename/facts.d.ts';
 
                             const libSource = [
-                                'declare class Editor {',
-                                'public setSQLInput(sql: string): void',
+                                'declare class ScriptingAPI {',
+                                'public push(sql: string, internal: boolean = true): void',
+                                '/* Hello world */ public pull(sql: string, internal: boolean = true): Promise<object>',
                                 '}',
-                                'declare const editor: Editor;',
-                                'declare const push: (sql: string) => void;'
+                                'declare const api: ScriptingAPI;',
                             ].join('\n');
 
                             monaco.languages.typescript.javascriptDefaults.addExtraLib(libSource, libUri);
@@ -102,7 +106,7 @@ export class DatabaseScriptingTab extends BernieComponent<DatabaseScriptingTabPr
                         defaultValue={l.fdh.get("code", "// fallback")}
                         onChange={l.onChange}
                         options={{
-                            fontFamily: "JetBrains Mono",
+                            // fontFamily: "JetBrains Mono",
                             fontLigatures: true
                         }}
                     />
